e5320f51d72b5d3e8ac4df1535b1e37527979819
Dave Schmid <dave@pulpfree.org>
2021-03-13 16:54:18 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -22,10 +22,6 @@ curl=( --silent --show-error --fail)
 fi
 
 
-
-
-# Curl Command
-
 if [ "$debug" = "true" ]
 then
 
@@ -55,7 +51,6 @@ then
 
 echo "api_type=$api_type"
 
-
 if [ "$backend_type" = "loki" ]
 then
 
@@ -76,7 +71,6 @@ echo "No Backend Configured"
 
 fi
 
-
 elif [ "$api_type" = "rest" ]
 then
 
@@ -96,7 +90,9 @@ then
 
 echo "backend_type=$backend_type"
 
-/usr/bin/stdbuf -oL /usr/bin/python /weatherflow-listener/weatherflow-listener.py | /weatherflow-listener/rest-influxdb.sh
+JSON='\n{"type":"listen_start", "device_id": "'"$rest_device_id"'", "id":"weatherflow_listener-start"}\n{"type":"listen_start_events", "station_id": "'"$rest_station_id"'", "id":"weatherflow_listener-start_events"}\n{"type":"listen_rapid_start", "device_id": "'"$rest_device_id"'", "id":"weatherflow_listener-rapid_start"}\n'
+
+echo -e "$JSON" | /weatherflow-listener/websocat_amd64-linux-static -n "wss://ws.weatherflow.com/swd/data?token=$rest_token" | /weatherflow-listener/rest-influxdb.sh
 
 else
 
@@ -121,8 +117,6 @@ while ( true ); do
   DELAY=$(echo "60-($after-$before)" | bc)
   sleep "$DELAY"
 done
-
-
 
 elif  [ "$backend_type" = "influxdb" ]
 then
